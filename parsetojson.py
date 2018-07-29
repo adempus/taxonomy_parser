f58ebdc73254e6af82f5a6c18e630dfbf4546da6
Jeff Morin <adempus@pop-os.localdomain>
2018-07-29 01:17:21 -0400

    Initial commit.

---
+++ parsetojson.py
@@ -0,0 +1,54 @@
+import regex
+import json
+
+categoryDict = {}
+
+def inFromFile(inPath):
+    ''':param inPath: path to the file being read from.
+       :returns a list containing lists of sequentially parsed categories.
+    '''
+    with open(inPath) as file:
+        return file.readlines()
+
+
+def parseData(contents):
+    ''':param contents: a list of lists containing sequentially parsed categories from file. '''
+    for cont in contents:
+        parsedStr = regex.sub('[0-9].* - ', repl="", string=cont)
+        if not '>' in parsedStr:
+            parsedStr = parsedStr.strip()
+            categoryDict[parsedStr] = {}
+        elif '>' in parsedStr:
+            splitCats = parsedStr.split('>')
+            categorize(categoryDict, splitCats)
+
+
+def categorize(dic, sequence=list()):
+    ''' :param dic: a nested dictionary of categories in categoryDict.
+        :param sequence: a list of strings ordered by category from input data.
+    '''
+    if len(sequence) > 0:
+        cat = sequence[0].strip()
+        if cat in dic:
+            dic = dic[cat]
+            categorize(dic, sequence[1:])
+        else:
+            dic[cat] = {}
+
+
+def outToFile(outPath):
+    ''':param outPath: path to the file being written to. '''
+    with open(outPath, 'w') as file:
+        file.write(
+            json.dumps(categoryDict, indent=10))
+
+
+def main():
+    contents = inFromFile('./res/taxonomies.txt')
+    # print(contents)
+    parseData(contents)
+    outToFile('./res/jsonOutput.json')
+
+
+if '__main__'==__name__:
+    main()
